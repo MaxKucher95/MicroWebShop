65ee65bc91275a9e61c9d29219db10274faab7dc
MaximilianKucher <m.kucher@gmx.de>
2020-05-08 15:57:34 +0200

    User microservice update für eureka

--- UserCore/src/main/java/de/hska/iwi/vslab/user/UserApplication.java
+++ UserCore/src/main/java/de/hska/iwi/vslab/user/UserApplication.java
@@ -2,10 +2,12 @@ package de.hska.iwi.vslab.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserApplication 
-{	
+{
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(UserApplication.class, args);
