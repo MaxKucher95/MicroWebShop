6cc3047e66e8b9d5edb706e6dbac779d97693f32
Nico <Nico.Wellermann@live.de>
2020-06-03 09:55:40 +0200

    Solved difference in slides and reality

--- CatalogueComposite/src/main/java/de/hska/iwi/vslab/catalogue/CatalogueApplication.java
+++ CatalogueComposite/src/main/java/de/hska/iwi/vslab/catalogue/CatalogueApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -13,6 +14,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableHystrix
 @EnableHystrixDashboard
 @RibbonClient("catalogue")
 public class CatalogueApplication {
